b0ddf7edaac6d45f7d6a3b7a8c994fd523c39310
Mikhail Malisov <malisov.mikhail@gmail.com>
2022-12-01 18:05:24 +0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,74 @@
+const { Server } = require('socket.io')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = new Server().listen(server)
+server.listen(3001)
+expressServer.get('/', function (request, response) {
+  response.sendFile(__dirname + '/app/index.html')
+})
+expressServer.get('/roomselector', function (request, response) {
+  response.sendFile(__dirname + '/app/index.html')
+})
+expressServer.get('/game', function (request, response) {
+  response.sendFile(__dirname + '/app/index.html')
+})
+
+const rooms = io.of('/').adapter.rooms
+const sids = io.of('/').adapter.sids
+
+io.of('/').adapter.on('create-room', (room) => {
+  console.log(`room ${room} was created`)
+})
+io.of('/').adapter.on('join-room', (room, id) => {
+  console.log(`socket ${id} has joined room ${room}`)
+})
+
+const getRoomsAndSizes = () => {
+  let allRooms = []
+  rooms.forEach((value, key) => {
+    allRooms.push({ name: key, size: value.size })
+  })
+  let userCreatedRooms = allRooms.filter((room) => room.name.includes('Room'))
+  return userCreatedRooms
+}
+
+var roomsData = {}
+
+io.on('connection', (socket) => {
+  socket.on('userLoggedIn', (user) => {
+    console.log(`User ${user} logged in`)
+    io.emit('roomsListUpdate', getRoomsAndSizes())
+    console.log(rooms)
+  })
+  socket.on('click', (values, room) => {
+    if (values[0] === 'reset') {
+      roomsData[room] = new Array(9).fill('')
+      console.log(`room ${room} was reset`)
+      io.to(room).emit('reset')
+    } else {
+      roomsData[room] = values
+      console.log(values)
+      io.to(room).emit('change', values)
+    }
+  })
+  socket.on('userJoinedRoom', (room, role) => {
+    socket.join(room)
+    io.emit('roomsListUpdate', getRoomsAndSizes())
+    if (role === 'spectator') {
+      console.log(roomsData[room])
+      console.log('SPECTATOR SOCKET', socket.id)
+      if (roomsData[room]) socket.emit('change', roomsData[room])
+    }
+    console.log(getRoomsAndSizes())
+  })
+  socket.on('disconnect', () => {
+    console.log(`Socket disconnected`)
+    io.emit('roomsListUpdate', getRoomsAndSizes())
+  })
+  socket.on('deleteRoom', (room) => {
+    console.log(`DELETING ROOM ${room}`)
+    io.in(room).socketsLeave(room)
+    console.log(getRoomsAndSizes())
+  })
+})
